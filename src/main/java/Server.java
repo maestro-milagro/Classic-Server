7e3a70cbfcddc65296671f680ec0732052dba9df
Sergey Korchagin <millagros77@gmail.com>
2023-06-08 12:50:43 +0300

    Functionality added

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,103 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Server {
+    int port = 8085;
+    private static final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+
+//    public static void main(String[] args) throws IOException {
+//        Server server = new Server();
+//
+//    }
+
+    public void start() {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                handle(serverSocket);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void handle(ServerSocket serverSocket) {
+        threadPool.submit(new Thread(() -> {
+            while (true) {
+                try (
+                        final var socket = serverSocket.accept();
+                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        final var out = new BufferedOutputStream(socket.getOutputStream());
+                ) {
+                    // read only request line for simplicity
+                    // must be in form GET /path HTTP/1.1
+                    final var requestLine = in.readLine();
+                    final var parts = requestLine.split(" ");
+
+                    if (parts.length != 3) {
+                        // just close socket
+                        continue;
+                    }
+
+                    final var path = parts[1];
+                    if (!validPaths.contains(path)) {
+                        out.write((
+                                "HTTP/1.1 404 Not Found\r\n" +
+                                        "Content-Length: 0\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                        out.flush();
+                        continue;
+                    }
+
+                    final var filePath = Path.of(".", "src\\public", path);
+                    final var mimeType = Files.probeContentType(filePath);
+
+                    // special case for classic
+                    if (path.equals("/classic.html")) {
+                        final var template = Files.readString(filePath);
+                        final var content = template.replace(
+                                "{time}",
+                                LocalDateTime.now().toString()
+                        ).getBytes();
+                        out.write((
+                                "HTTP/1.1 200 OK\r\n" +
+                                        "Content-Type: " + mimeType + "\r\n" +
+                                        "Content-Length: " + content.length + "\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                        out.write(content);
+                        out.flush();
+                        continue;
+                    }
+
+                    final var length = Files.size(filePath);
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    Files.copy(filePath, out);
+                    out.flush();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }));
+    }
 }
