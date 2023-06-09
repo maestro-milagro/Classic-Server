b8bdb5a070f3c83cc112adf116d332234078a325
Sergey Korchagin <millagros77@gmail.com>
2023-06-09 12:44:46 +0300

    Handlers added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,21 +9,133 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         final var server = new Server();
-        server.start();
         // код инициализации сервера (из вашего предыдущего ДЗ)
 
         // добавление хендлеров (обработчиков)
-//        server.addHandler("GET", "/messages", new Handler() {
-//            public void handle(Request request, BufferedOutputStream responseStream) {
-//                // TODO: handlers code
-//            }
-//        });
-//        server.addHandler("POST", "/messages", new Handler() {
-//            public void handle(Request request, BufferedOutputStream responseStream) {
-//                // TODO: handlers code
-//            }
-//        });
-//
-//        server.listen(9999);
+        server.addHandler("GET", "/spring.png", (request, responseStream) -> {
+            try (
+                    final var out = responseStream;
+            ) {
+                final var path = request.getPath();
+                final var filePath = Path.of(".", "src\\public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    return;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+        server.addHandler("POST", "/messages", (request, responseStream) -> {
+            try (
+                    final var out = responseStream;
+            ) {
+                final var path = request.getPath();
+                final var filePath = Path.of(".", "src\\public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    return;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+        server.addHandler("GET", "/spring.svg", (request, responseStream) -> {
+            try (
+                    final var out = responseStream;
+            ) {
+                final var path = request.getPath();
+                final var filePath = Path.of(".", "src\\public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    return;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+
+        server.listen(8085);
     }
 }
---
+++ src/main/java/Request.java
@@ -0,0 +1,44 @@
+public class Request {
+    private String method;
+    private String header;
+    private String body;
+    private String path;
+    public Request(String method, String path, String body, String header){
+        this.body = body;
+        this.header = header;
+        this.method = method;
+        this.path = path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
